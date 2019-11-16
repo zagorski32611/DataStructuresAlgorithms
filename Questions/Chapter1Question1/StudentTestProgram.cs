953eccfc0861c3c1c471c3c0d5905d5109191289
Joe <j.j.zagorski@gmail.com>
2019-11-16 13:27:53 -0500

    chapter1 review

---
+++ Questions/Chapter1Question1/StudentTestProgram.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Diagnostics;
+using System.Collections;
+
+namespace DatastructuresandAlgorithms
+{
+    public class StudentTestProgram
+    {
+        public StudentTestProgram()
+        {
+
+        }
+
+    }
+
+    public class StudentCollection : CollectionBase
+    {
+        public void Add(Object name, Object test_number)
+        {
+            //InnerList.Add(name, test_number);
+        }
+    }
+
+
+}
