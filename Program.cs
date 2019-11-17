16734a217b1751a458543ee1ed9bb34bf3339c13
Joe <j.j.zagorski@gmail.com>
2019-11-17 15:18:00 -0500

    added question 2 and 3

--- Program.cs
+++ Program.cs
@@ -9,9 +9,12 @@ namespace DatastructuresandAlgorithms
             // Please choose from the list of classes below, copy and paste them here.
             // Then debug as normal.
             
-            Question2 q2 = new Question2();
-            Question2.usingCollection();
+            //Question2 q2 = new Question2();
+            //Question2.usingCollection();
 
+            Question3 q3 = new Question3();
+            q3.customcollectTime();
+            q3.ArrayListTime();
         }
     }
 }
---
+++ Questions/Chapter1Question3/Chapter1Question3.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Diagnostics;
+
+namespace DatastructuresandAlgorithms
+{
+    public class Question3
+    {
+        Timing time = new Timing();
+        public TimeSpan customcollectTime()
+        {
+            time.StartTime();
+            CustomCollection collect = new CustomCollection();
+            for (int i = 0; i <= 1000; i++)
+            {
+                collect.Add(i);
+            }
+            time.StopTime();
+            Console.WriteLine($"Custom Collection time: {time.Result()}");
+            return time.Result();
+        }
+
+        public TimeSpan ArrayListTime()
+        {
+            time.StartTime();
+            for (int i = 0; i <= 1000; i++)
+            {
+                ArrayList array = new ArrayList();
+                array.Add(i);
+            }
+            time.StopTime();
+            Console.WriteLine($"Array List Time: {time.Result()}");
+            return time.Result();
+        }
+    }
+}
+
+
+
+/*
+
+            int[] nums = new int[10000];
+            BuildArray(nums);
+            Timing timeObject = new Timing();
+
+            timeObject.StartTime();
+            timeObject.StopTime();
+
+            var results = timeObject.Result();
+
+            Console.WriteLine($"Total Time, .NET: {results.TotalMilliseconds} milliseconds");
+            Console.WriteLine($"Total Time, .NET: {results.TotalSeconds} seconds");
+            Console.WriteLine($"Total Time, .NET: {results.Ticks} ticks lol");
+            return true;
+            */
