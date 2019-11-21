f9cf2939435087b19877b2a0f7eadebee7d89424
Joe <j.j.zagorski@gmail.com>
2019-11-21 06:00:37 -0500

    added selection sort

--- Chapter3/ArrayClassTestBed.cs
+++ Chapter3/ArrayClassTestBed.cs
@@ -5,13 +5,13 @@ namespace DatastructuresandAlgorithms
     public class CArray
     {
         private int[] arr;
-        private int upper;
+        private int arrSizeMinusOne;
         private int numElements;
 
         public CArray(int size)
         {
             arr = new int[size];
-            upper = size - 1;
+            arrSizeMinusOne = size - 1;
             numElements = 0;
         }
 
@@ -23,7 +23,7 @@ namespace DatastructuresandAlgorithms
 
         public void DisplayElements()
         {
-            for (int i = 0; i <= upper; i++)
+            for (int i = 0; i <= arrSizeMinusOne; i++)
             {
                 Console.WriteLine(arr[i] + " ");
             }
@@ -31,7 +31,7 @@ namespace DatastructuresandAlgorithms
 
         public void Clear()
         {
-            for (int i = 0; i <= upper; i++)
+            for (int i = 0; i <= arrSizeMinusOne; i++)
             {
                 arr[i] = 0;
             }
@@ -40,38 +40,56 @@ namespace DatastructuresandAlgorithms
 
         public static void newArray()
         {
-            // This code does not randomly assign an integer to an index. There's nothing to search, it's in order:
-            //CArray nums = new CArray(50);
-            //for (int i = 0; i <= 49; i++)
-            //{
-            //    nums.Insert(i);
-            //}
-            //nums.DisplayElements();
-
             // This code with randomly assign numbers to the array:
-            CArray nums = new CArray(10);
+            CArray nums = new CArray(5);
             Random rnd = new Random(100);
 
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 5; i++)
             {
                 nums.Insert((int)(rnd.NextDouble() * 100));
             }
             
-            Console.WriteLine("Before Sorting:");
+            Console.WriteLine("New Array:");
             nums.DisplayElements();
 
-            Console.WriteLine("During Sorting:");
+            Console.WriteLine("Bubble Sorting:");
             nums.BubbleSort();
 
-            Console.WriteLine("After Dorting:");
+            Console.WriteLine("After Bubble Sorting:");
             nums.DisplayElements();
+
             
+            for (int i = 0; i < 5; i++)
+            {
+                nums.Insert((int)(rnd.NextDouble() * 100));
+            }
+
+
+            Console.WriteLine("New array:");
+            nums.DisplayElements();
+
+            Console.WriteLine("Selection Sort:");
+            nums.SelectionSort();
+            nums.DisplayElements();
+
+            for (int i = 0; i < 5; i++)
+            {
+                nums.Insert((int)(rnd.NextDouble() * 100));
+            }
+            Console.WriteLine("New Array:");
+            nums.DisplayElements();
+
+            Console.WriteLine("Insertion Sort:");
+            nums.InsertionSort();
+            nums.DisplayElements();
+
+
         }
 
         public void BubbleSort()
         {
             int temp;
-            for (int outer = upper; outer >= 1; outer--)
+            for (int outer = arrSizeMinusOne; outer >= 1; outer--)
             {
                 for (int inner = 0; inner <= outer - 1; inner++)
                 {
@@ -86,6 +104,44 @@ namespace DatastructuresandAlgorithms
             }
         }
 
-        
+
+        public void SelectionSort()
+        {
+            int smallest;
+            int temp;
+
+            for (int outer = 0; outer <= arrSizeMinusOne; outer++)
+            {
+                smallest = outer;
+                for(int inner = outer + 1; inner <= arrSizeMinusOne; inner++)
+                {
+                    if(arr[inner] < arr[smallest])
+                    {
+                        smallest = inner;
+                    }
+
+                    temp = arr[outer];
+                    arr[outer] = arr[smallest];
+                    arr[smallest] = temp;
+                }
+            }
+        }
+
+        public void InsertionSort()
+        {
+            int inner;
+            int temp;
+            for (int outer = 1; outer <= arrSizeMinusOne; outer++)
+            {
+                temp = arr[outer];
+                inner = outer;
+                while (inner > 0 && arr[inner - 1] >= temp)
+                {
+                    arr[inner] = arr[inner - 1];
+                    inner -= 1;
+                }
+                arr[inner] = temp;
+            }
+        }
     }
 }
